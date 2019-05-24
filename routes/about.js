00c8c747eb202de8f7a6bca4bc1049948f696685
jeshc <hcjesus@gmail.com>
2019-05-24 14:24:55 -0500

    Segunda cosa

--- routes/about.js
+++ routes/about.js
@@ -7,6 +7,6 @@ router.get('/', function(req, res, next) {
   });
   });
 
-
+  
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'
-  });
+  res.render('index', { title: 'Express'});
   });
 
+
+  router.get('/about/pagina1', function(req, res, next) {
+    res.render('pagina1', { title: 'Express'});
+    });
 
 
 module.exports = router;
