86339833fbd47dfabb2706241b34e0e3130c85cc
zarbok <andresolivier@hotmail.com>
2019-05-27 21:57:41 -0500

    avance de pagina de definiciones

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
-var apartado1Router = require('./routes/apartado1'); 
+var apartado1Router = require('./routes/apartado1');
+var definicionesRouter = require('./routes/definiciones');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/apartado1',apartado1Router);
+app.use('/definiciones',definicionesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
